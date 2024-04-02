906b1b9f295af106cbb31636701f7e0e337a2bea
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-04-02 19:52:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function showAlert(){
+    function tampil(){
+        alert("MyFlorist adalah toko bunga yang bisa membuat bucket sesuai request anda.")
+    }
+
+    function show(){
+        alert("info lebih lanjut 085897564347 atau instagram kami @MyFlorist")
+    }
+
+    tampil(); // Memanggil fungsi tampil()
+    show();   // Memanggil fungsi show()
+}
